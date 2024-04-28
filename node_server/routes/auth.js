232116c4e80d284c1c443ea7efcdc8dcd4310244
Jacky Zhang <143105453+jackyz2@users.noreply.github.com>
2024-04-28 14:42:47 -0500

    Update auth.js

--- node_server/routes/auth.js
+++ node_server/routes/auth.js
@@ -4,7 +4,7 @@ const controllers = require('../controllers');
 
 router.post('/signup', controllers.signUp)
 router.post('/login', controllers.login);
-router.post('/newactoken', controllers.createAccessToken);
-router.post('/newrftoken', controllers.createRefreshToken);
+router.post('/newactoken', controllers.newAccessToken);
+router.post('/newrftoken', controllers.newRefreshToken);
 router.post('/logout', controllers.logout);
 module.exports = router;
