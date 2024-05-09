1f021dbbf95e02c6110672b57d3d57c0d34c2d0d
Jacky Zhang <jackyz2@illinois.edu>
2024-05-09 13:18:42 +0800

    added score screen and progress bar, fixed the choice selection bug

--- lib/pages/lesson1.dart
+++ lib/pages/lesson1.dart
@@ -2,21 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/question_model.dart';
 import 'package:flutter_application_1/services/api.dart';
 import 'package:flutter_application_1/pages/learningpage.dart';
+import 'package:flutter_application_1/pages/scorescreen.dart';
 
-int? selectedAnswerIndex;
-  int questionIndex = 0;
-  int idx = 0;
-  int score = 0;
-  bool checked = false;
-  String buttonText = "Check Answer";
-  Future<List<Question>>? fetchedQuestions;
-  List<Question>? questions;
+Future<List<Question>>? fetchedQuestions;
+List<Question>? questions;
 
 Future<List<Question>> fetchQuizQuestions() async {
-    var refreshToken = await API.currentUserData.read(key: 'refreshToken');
-    var data = {"refreshToken": refreshToken};
-    return API.parseQ(data);
+  var refreshToken = await API.currentUserData.read(key: 'refreshToken');
+  var data = {"refreshToken": refreshToken};
+  return API.parseQ(data);
 }
+
+class ProgressBar extends StatefulWidget {
+  final int currentIndex;
+  final int totalQuestions;
+
+  const ProgressBar({
+    Key? key,
+    required this.currentIndex,
+    required this.totalQuestions,
+  }) : super(key: key);
+
+  _ProgressBarState createState() => _ProgressBarState();
+}
+
+class _ProgressBarState extends State<ProgressBar> {
+  @override
+  Widget build(BuildContext context) {
+    double progress = widget.currentIndex / widget.totalQuestions;
+    return LinearProgressIndicator(
+      value: progress,
+      backgroundColor: Colors.grey[300],
+      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+    );
+  }
+}
+
 class AnswerCard extends StatelessWidget {
   const AnswerCard({
     super.key,
@@ -25,6 +46,7 @@ class AnswerCard extends StatelessWidget {
     required this.currentIndex,
     required this.correctAnswer,
     required this.selectedAnswerIndex,
+    required this.check
   });
 
   final String question;
@@ -32,77 +54,51 @@ class AnswerCard extends StatelessWidget {
   final String correctAnswer;
   final int? selectedAnswerIndex;
   final int currentIndex;
+  final bool check;
 
   @override
   Widget build(BuildContext context) {
     bool isCorrectAnswer = question == correctAnswer;
     bool isWrongAnswer = !isCorrectAnswer && isSelected;
     return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 10.0,
-      ),
-      
-      child: selectedAnswerIndex != null
-          // if one option is chosen
-          ? Container(
-              height: 70,
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: isCorrectAnswer
+        padding: const EdgeInsets.symmetric(
+          vertical: 10.0,
+        ),
+        child: Container(
+          height: 70,
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.lightBlue[200] : Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: selectedAnswerIndex != null && check
+                  ? isCorrectAnswer
                       ? Colors.green
                       : isWrongAnswer
                           ? Colors.red
-                          : Colors.grey,
+                          : Colors.grey
+                  : Colors.white,
+            ),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  question,
+                  style: const TextStyle(fontSize: 16, color: Colors.black),
                 ),
               ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      question,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        color: Colors.red
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  isCorrectAnswer
+              const SizedBox(height: 10),
+              selectedAnswerIndex == null || !check
+                  ? const SizedBox.shrink()
+                  : isCorrectAnswer
                       ? buildCorrectIcon()
                       : isWrongAnswer
                           ? buildWrongIcon()
                           : const SizedBox.shrink(),
-                ],
-              ),
-            )
-          // If no option is selected
-          : Container(
-              height: 70,
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Colors.white,
-                ),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      question,
-                      style: const TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-    );
+            ],
+          ),
+        ));
   }
 }
 
@@ -129,14 +125,13 @@ class QuizFetchScreen extends StatefulWidget {
   @override
   State<QuizFetchScreen> createState() => _QuizFetchScreenState();
 }
-  
+
 class _QuizFetchScreenState extends State<QuizFetchScreen> {
-  @override 
+  @override
   Widget build(BuildContext context) {
     //final question = questions[0];
-    
-      return Container(
-        height: MediaQuery.of(context).size.height,
+    return Container(
+      height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
       decoration: const BoxDecoration(
         gradient: LinearGradient(
@@ -145,8 +140,7 @@ class _QuizFetchScreenState extends State<QuizFetchScreen> {
           end: Alignment.bottomRight,
         ),
       ),
-      child:
-      FutureBuilder<List <Question>>(
+      child: FutureBuilder<List<Question>>(
         future: fetchQuizQuestions(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -157,7 +151,7 @@ class _QuizFetchScreenState extends State<QuizFetchScreen> {
             return Center(child: Text('Error: ${snapshot.error.toString()}'));
           } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
             // Data is fetched successfully and the list is not empty
-            //Question currQuestion = snapshot.data![questionIndex]; 
+            //Question currQuestion = snapshot.data![questionIndex];
             //bool isLastQuestion = questionIndex == snapshot.data!.length - 1;
             questions = snapshot.data!;
             return QuizScreen();
@@ -167,7 +161,7 @@ class _QuizFetchScreenState extends State<QuizFetchScreen> {
           }
         },
       ),
-      );
+    );
   }
 }
 
@@ -178,81 +172,99 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-  @override 
+  int? selectedAnswerIndex;
+  int questionIndex = 0;
+  int idx = 0;
+  int score = 0;
+  bool checked = false;
+  String buttonText = "Check Answer";
+  @override
   Widget build(BuildContext context) {
-Question currQuestion = questions![questionIndex];
-return Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    currQuestion.questionTitle,
-                    style: const TextStyle( 
-                      fontSize: 25,
-                      color: Colors.white
-                    ),
-                    textAlign: TextAlign.center,
-                  ), // Display the question
-                  ListView.builder( 
-                    shrinkWrap: true,
-                    itemCount: currQuestion.options.length,
-                    itemBuilder: (context, index) {
-                      return GestureDetector( 
-                        onTap: selectedAnswerIndex == null ?() { 
-                          selectedAnswerIndex = index;
-                          idx = index;
-                         } : null,
-                        child: AnswerCard( 
-                          currentIndex: index,
-                          question: currQuestion.options[index],
-                          isSelected: selectedAnswerIndex == index,
-                          selectedAnswerIndex: selectedAnswerIndex,
-                          correctAnswer: currQuestion.answer,
-                        )
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                        height: 30,
-                      ),
-                      GestureDetector(
-                        onTap: selectedAnswerIndex == null ?() { 
-                          if(!checked){
-                            if(currQuestion.options[idx] == currQuestion.answer) {
-                              ++score;
-                            }
-                            checked = true;
-                            buttonText = "Continue";
-                          }
-                          setState(() {});
-                         } : () {
-                            ++questionIndex;
-                            selectedAnswerIndex = null;
-                            buttonText = "Check Answer";
-                            checked = false;
-                            currQuestion = questions![questionIndex]; 
-                            setState(() {});
-                          },
-                          
-                        child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.lightBlue[200],
-                          ),
-                          child: Center(
-                            child: Text(
-                              buttonText,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 16),
-                            ),
-                          ),
-                        ),
-                      ),
-                
-                ],
+    Question currQuestion = questions![questionIndex];
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ProgressBar(
+              currentIndex: questionIndex, totalQuestions: questions!.length),
+          Text(
+            currQuestion.questionTitle,
+            style: const TextStyle(fontSize: 25, color: Colors.white),
+            textAlign: TextAlign.center,
+          ), // Display the question
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: currQuestion.options.length,
+            itemBuilder: (context, index) {
+              bool isSelected = selectedAnswerIndex == index;
+              return GestureDetector(
+                  onTap: checked
+                      ? null
+                      : () {
+                          setState(() {
+                            selectedAnswerIndex = index;
+                          });
+                        },
+                  child: AnswerCard(
+                    currentIndex: index,
+                    question: currQuestion.options[index],
+                    isSelected: isSelected,
+                    selectedAnswerIndex: selectedAnswerIndex,
+                    correctAnswer: currQuestion.answer,
+                    check: checked
+                  ));
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onTap: !checked
+                ? () {
+                    if (selectedAnswerIndex != null) {
+                      if (currQuestion.options[selectedAnswerIndex!] == currQuestion.answer) {
+                        ++score;
+                      }
+                    }
+                    setState(() {
+                      checked = true;
+                      buttonText = "Continue";
+                    });
+                  }
+                : questionIndex == questions!.length - 1
+                    ? () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                                builder: (context) => ScoreScreen(score: score,)),
+                        );
+                      }
+                    : () {
+                        setState(() {
+                          ++questionIndex;
+                          selectedAnswerIndex = null;
+                          buttonText = "Check Answer";
+                          checked = false;
+                          currQuestion = questions![questionIndex];
+                        });
+                      },
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.lightBlue[200],
               ),
-      );
+              child: Center(
+                child: Text(
+                  buttonText,
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
